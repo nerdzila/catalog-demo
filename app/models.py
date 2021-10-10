ecf6e04a8d17c30dd107e0906b7cbaf1b3df0615
Alfonso Sanchez <nerdzila@gmail.com>
2021-10-10 17:22:53 -0500

    Adds Product DB Model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, Numeric
 
 from .database import Base
 
@@ -10,3 +10,14 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_admin = Column(Boolean, default=True)
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sku = Column(String, unique=True, index=True)
+    name = Column(String)
+    price = Column(Numeric)
+    brand = Column(String)
+    description = Column(String)
