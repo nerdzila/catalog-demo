c0ef4cb809d85b26e8197154ef5ff616f76f6db3
Alfonso Sanchez <nerdzila@gmail.com>
2021-10-09 21:53:35 -0500

    Adds SQLAlchemy/sqlite configuration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from .database import SessionLocal, engine
+
+
+# TODO: delegate DB initalization to migrations library
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
+# ==================
+# Dependencies
+# ==================
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# ==================
+# Endpoints
+# ==================
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
