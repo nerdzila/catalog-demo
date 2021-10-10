f6fe95384315f9de6a5d872bfc5035f20f7bd239
Alfonso Sanchez <nerdzila@gmail.com>
2021-10-10 17:23:55 -0500

    Adds Pydantic schemas for Products
    * Also changes the User schema docs to be consistent with the new ones

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from pydantic import BaseModel
 
 
@@ -12,19 +14,15 @@ class Token(BaseModel):
 
 # ==================================================================
 # User Schemas
-#
-# Different User Schemas for GET (UserOut) and
-# PUT/POST (UserIn) endpoints, DELETEd users get their
-# own schema
 # ==================================================================
 class UserBase(BaseModel):
-    """Email and Admin status: always relevant"""
+    """Properties that most user schemas use"""
     email: str
     is_admin: bool
 
 
 class UserIn(UserBase):
-    """Password: only relevant when creating/updating"""
+    """The password is only relevant for PUT/POST requests"""
     password: str
 
 
@@ -40,6 +38,36 @@ class UserDeleted(BaseModel):
     """For deleted users we return only the ID"""
     id: int
 
+
 # ==================================================================
 # Product Schemas
 # ==================================================================
+class ProductBase(BaseModel):
+    """Commonly used properties"""
+    sku: str
+    name: str
+    price: Decimal
+    brand: str
+
+
+class ProductIn(ProductBase):
+    """POST/PUT endpoints should have all non-ID props"""
+    description: str
+
+
+class ProductOut(ProductBase):
+    """When retrieving a list of products, add the ID"""
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ProductOutDetails(ProductOut):
+    """When looking at a product detail, add the description"""
+    description: str
+
+
+class ProductDeleted(BaseModel):
+    """DELETE endpoint gets only the ID"""
+    id: int
