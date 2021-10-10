d0c1f92b0c39108065ce678f452b3bc88e923fef
Alfonso Sanchez <nerdzila@gmail.com>
2021-10-10 16:57:34 -0500

    Deleted unused token schema, tidy schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,22 @@
-from typing import Optional
-
 from pydantic import BaseModel
 
 
+# ==================================================================
+# General purpose schemas
+# ==================================================================
+class Token(BaseModel):
+    """Schema to return JWT token"""
+    access_token: str
+    token_type: str
+
+
+# ==================================================================
+# User Schemas
+#
 # Different User Schemas for GET (UserOut) and
 # PUT/POST (UserIn) endpoints, DELETEd users get their
 # own schema
+# ==================================================================
 class UserBase(BaseModel):
     """Email and Admin status: always relevant"""
     email: str
@@ -28,12 +39,3 @@ class UserOut(UserBase):
 class UserDeleted(BaseModel):
     """For deleted users we return only the ID"""
     id: int
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
